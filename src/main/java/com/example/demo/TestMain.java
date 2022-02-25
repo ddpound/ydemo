b43a248b039d29a1d7ded5269cea4ee92878e454
dpound <dpound@naver.com>
2022-02-25 17:22:29 +0900

    테스트

---
+++ src/main/java/com/example/demo/TestMain.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class TestMain {
+    public static void main(String[] args) {
+        // Animal 로 할 예정
+
+
+
+
+    }
+
+}
---
+++ src/main/java/com/example/demo/animal/Animal.java
@@ -0,0 +1,4 @@
+package com.example.demo.animal;
+
+public class Animal {
+}
